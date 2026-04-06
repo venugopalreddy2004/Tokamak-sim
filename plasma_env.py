0a069e1688a83a0326f274cc55ebeaaae6324923
venugopalreddy2004 <raamigaani2004@gmail.com>
2026-04-06 23:36:11 +0530

    improving reward function

--- plasma_env.py
+++ plasma_env.py
@@ -2,7 +2,6 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 import random
-from queue import Queue
 
 class TokamakEnv(gym.Env):
     
@@ -11,12 +10,16 @@ class TokamakEnv(gym.Env):
         
         # Constraints
         self.dt = 0.001
-        self.max_z = 1
-        self.alpha = 30
+        self.max_z = 0.3
+        self.alpha = 1.0
         self.L = 1.2
         self.R = 0.78
         self.max_voltage = 150.0
         self.max_steps = 2000
+        
+        self.norm_z = 1.0
+        self.norm_dz = 10.0
+        self.norm_I = 100.0
         
         #definin action space [V]
         self.action_space = spaces.Box(low=-1.0 ,high=1.0 ,shape=(1,),dtype=np.float32)
@@ -24,30 +27,30 @@ class TokamakEnv(gym.Env):
         #defn observation space [Z, dZ, I]
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3,),dtype=np.float32)
         
-    def reset(self, *, seed = None, options = None):
+    def reset(self, seed = None, options = None):
         super().reset(seed=seed)
 
         self.state = np.random.uniform(low=-0.01, high=0.01, size=(3,)).astype(np.float32)
         #this will amount to the delay to providing required voltage to the reactor 
-        aq = Queue(maxsize=8)
-        for _ in range(8):
-            aq.put(0.0)
-        self.action_queue = aq
-        self.gamma = 49.0
+        self.action_queue = [0]*8
+        self.gamma = 15.0
         self.steps = 0
         
-        return self.state, {}
+        return self._get_norm_state(), {}
 
+    def _get_norm_state(self):
+        z, dz, I = self.state
+        norm = np.array([z / self.norm_z, dz / self.norm_dz, I / self.norm_I], dtype=np.float32)
+        return np.clip(norm, -5.0, 5.0)
     
     def step(self, action):
         z, dz, In = self.state
         
-        delayed_action = self.action_queue.get()
-        self.action_queue.put(float(action[0]))
+        self.action_queue.append(float(action[0]))
+        delayed_action = self.action_queue.pop(0)
         V = delayed_action * self.max_voltage
-        
-        fluc = 0.07
-        self.gamma += fluc
+
+        self.gamma += 0.02
         
         dI = (V - self.R * In)/(self.L)
         In1 = In + (dI*self.dt)
@@ -62,12 +65,12 @@ class TokamakEnv(gym.Env):
         truncated = bool(self.steps >= self.max_steps)
         
         if terminated:
-            reward = -1000
+            reward = -100
         else:
-            reward = 1.0 - (10.0 * (z1**2)) - (0.1 * (dz1**2)) - (0.001 * (action[0]**2))
-        self.state = np.array([z1, dz1, In1], dtype=np.float32)
+            reward = 1.0 - (20.0 * (z1**2)) - (0.7 * (dz1**2)) - (0.01 * (action[0]**2))
         
-        return self.state, float(reward), terminated, truncated, {}
+        self.state = np.array([z1, dz1, In1], dtype=np.float32)
+        return self._get_norm_state(), float(reward), terminated, truncated, {}
         
 
 if __name__ == "__main__":
